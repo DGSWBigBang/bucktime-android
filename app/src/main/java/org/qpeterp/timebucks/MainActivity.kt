8b2e0a2ec128315283a2cbf095b812c4fcc0c058
ssh <seoseunghun3@gmail.com>
2023-10-21 20:59:31 +0900

    joinview ui ver1

--- app/src/main/java/org/qpeterp/timebucks/MainActivity.kt
+++ app/src/main/java/org/qpeterp/timebucks/MainActivity.kt
@@ -15,6 +15,10 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
+
+
+
 //        Log.d("onCreate", "complete")
 //        val cafeInformation = serverCommu.LoadCafe(this) { arr ->
 //            binding.data.text = "${arr.count()}"
