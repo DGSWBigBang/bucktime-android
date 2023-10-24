c306809368c460112f1985f6bfc18e599c1f2719
qpeterp <132974777+qpeterp@users.noreply.github.com>
2023-10-24 23:26:17 +0900

    mainBranch commit for plus main and login ver_2

--- app/src/main/java/org/qpeterp/timebucks/MainActivity.kt
+++ app/src/main/java/org/qpeterp/timebucks/MainActivity.kt
@@ -1,28 +1,14 @@
 package org.qpeterp.timebucks
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import org.qpeterp.timebucks.databinding.ActivityStartBinding
-import org.qpeterp.timebucks.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityStartBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        binding.idSigninButton.setOnClickListener {
-            try {
-                val intent = Intent(this, MainMainActivity::class.java)
-                startActivity(intent)
-                Log.d("fdafda","complete")
-            } catch (e: Exception) {
-                Log.d("asdfasdf", "${e}")
-            }
-
-        }
 
     }
 }
